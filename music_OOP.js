f5e6f71c241da5a0ff85947e2713e63f66343fda
Arjun Lall <lallarjun@gmail.com>
2017-06-02 16:04:58 +0000

    Basic music library using OOP

--- music_OOP.js
+++ music_OOP.js
@@ -1,44 +1,65 @@
-// Work incrementally but by the end of it you should have defined multiple prototype functions:
-
-// Library
-// Playlist
-// Track
-// This one will be slightly different from the Week 1 in that it will have the following functionality:
-
-// A Library has a name and a creator (both strings) done
-// These are the only things required in order to create a music library
-// A Library has many playlists (starts as an empty array) done
-// Playlist objects can be added to an instance of a library
-// Each Playlist has a name which is required upon creation
-// A Playlist also has many tracks
-// A Track has a title, a rating (an integer from 1 to 5) and a length (integer in seconds) all of which are required when creating a new track
-// Each Playlist also has an overallRating function which will calculate the rating by averaging the rating of its tracks
-// Each Playlist also has a totalDuration function which will sum the duration of all of its tracks
-// Once defined, your code should use new to instantiate these functions to create instances of one library and then add new tracks and playlists to the library.
 
 class Playlist {
   constructor(name) {
-    this.
+    this.name = name;
+    this.tracks = [];
   }
-// Each Playlist also has an overallRating function which will calculate the rating by averaging the rating of its tracks
-// Each Playlist also has a totalDuration function which will sum the duration of all of its tracks
+//calculate the rating by averaging the rating of its tracks
+  overallRating() {
+    let sumRating = 0;
+    let avgRating = 0;
+    this.tracks.forEach((track) => {
+      sumRating += track.rating;
+    })
+    avgRating = sumRating/this.tracks.length;
+    return avgRating;
+  }
+//sum the duration of all of its tracks
+  totalDuration() {
+    let sumDuration = 0;
+    this.tracks.forEach((track) => {
+      sumDuration += track.length;
+    })
+    return sumDuration;
+  }
+
+  addTrack(track) {
+    this.tracks.push(track);
+  }
 
 
 }
 
+// A Track has a title, a rating (an integer from 1 to 5) and a length (integer in seconds)
 class Track {
   constructor(title, rating, length) {
-// A Track has a title, a rating (an integer from 1 to 5) and a length (integer in seconds) all of which are required when creating a new track
-
+    this.title = title;
+    this.rating = rating;
+    this.length = length;
   }
 }
 
 class Library {
   constructor(name, creator) {
-    this.
-    this._playlists = [];
-
+    this.name = name;
+    this.creator = creator;
+    this.playlists = [];
   }
+  addPlaylist(playlist) {
+    this.playlists.push(playlist)
 //add playlist to add playlist object to library
+  }
 }
 
+const track = new Track('Arjun', 10, 200);
+const track2 = new Track('Bill', 20, 200);
+
+const playlist = new Playlist("Arjun's hits")
+const library = new Library("Arjun's Library", "Arjun")
+playlist.addTrack(track);
+playlist.addTrack(track2);
+library.addPlaylist(playlist);
+console.log(playlist);
+console.log(playlist.overallRating());
+console.log(playlist.totalDuration());
+console.log(library);
